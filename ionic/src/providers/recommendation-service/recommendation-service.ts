90c39b5ddc65c1cf4a28e2044d1f7e8a64d2e2e0
potatoshane <li2137@purdue.edu>
2017-10-19 23:45:13 -0400

    Hardcoded Recommendation Service

--- ionic/src/providers/recommendation-service/recommendation-service.ts
+++ ionic/src/providers/recommendation-service/recommendation-service.ts
@@ -3,36 +3,57 @@ import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 /*
-  Generated class for the RecommendationServiceProvider provider.
+ Generated class for the RecommendationServiceProvider provider.
 
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular DI.
-*/
+ See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+ for more info on providers and Angular DI.
+ */
 @Injectable()
 export class RecommendationServiceProvider {
 
   recommendationsUrl = 'http://localhost:3000/consumables/get/1';
 
-  constructor(public http: Http) {
+  constructor(public http:Http) {
     console.log('Hello RecommendationServiceProvider Provider');
   }
 
-  data: any;
+  data:any;
 
   getAllItems(answers) {
-    if (this.data) {
-      return Promise.resolve(this.data);
-    }
-
-    return new Promise(resolve => {
-      //noinspection TypeScriptUnresolvedFunction
-      this.http.get(this.recommendationsUrl)
-        .map(res => res.json())
-        .subscribe(data => {
-          this.data = data.data;
-          resolve(this.data);
-        });
-    });
+    return {
+      _id: 24,
+      name: "Caffe Latte",
+      description: "Espresso and steamed milk, nothing beats this classic! Add flavors to taste",
+      price: 4,
+      jslImage: "https://www.deafcancoffee.com/images/portfolio/IMG_6369.JPG",
+      itemImage: "https://www.deafcancoffee.com/images/portfolio/IMG_6369.JPG",
+      caffeine: true,
+      modifiers: [
+        "mocha",
+        "vanilla",
+        "ice",
+        "no ice"
+      ],
+      size: [
+        "small",
+        "medium",
+        "large"
+      ],
+      __v: 0
+    };
+    //if (this.data) {
+    //  return Promise.resolve(this.data);
+    //}
+    //
+    //return new Promise(resolve => {
+    //  //noinspection TypeScriptUnresolvedFunction
+    //  this.http.get(this.recommendationsUrl)
+    //    .map(res => res.json())
+    //    .subscribe(data => {
+    //      this.data = data.data;
+    //      resolve(this.data);
+    //    });
+    //});
   }
 
 }
