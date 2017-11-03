160f7590577ab202ae039592ff14772fdc906b3a
potatoshane <li2137@purdue.edu>
2017-11-03 14:14:33 -0400

    Added Checkout Service

---
+++ ionic/src/providers/checkout-service/checkout-service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the CheckoutService provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular DI.
+*/
+@Injectable()
+export class CheckoutService {
+
+  items = [];
+
+  constructor(public http: Http) {
+    console.log('Hello CheckoutService Provider');
+  }
+
+  getCart() {
+    return this.items;
+  }
+
+  addItem(item) {
+    this.items.push(item)
+    return this.items;
+  }
+
+  removeItem(item) {
+    var index = this.items.indexOf(item, 0);
+    if (index > -1) {
+      this.items.splice(index, 1);
+    }
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+}
